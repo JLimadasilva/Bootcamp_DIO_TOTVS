f4d61277744d9b2f0e890071a9cb5868a5c85766
Joel Lima da Silva <105734951+JLimadasilva@users.noreply.github.com>
2026-04-20 23:51:20 -0300

    Adicionando estudos sobre ESTRUTURAS DE REPETIÇÂO

---
+++ aulas/estruturas_condicionais_repeticao/indentacao_e_blocos.py
@@ -0,0 +1,10 @@
+def sacar(valor):
+    saldo = 500
+
+    if saldo >= valor:
+        print("Valor sacado")
+        print("retire o seu dinheiro na boca do caixa")
+    print("Obrigadopor ser nosso cliente")
+
+sacar(1000)
+
