1cbfdc190c01d70ddd591a055509f588d0a67f3e
Joel Lima da Silva <105734951+JLimadasilva@users.noreply.github.com>
2026-04-22 23:07:43 -0300

    Adicionando o estudo de manipulação de Strings

---
+++ aulas/manipulando_strings/interpolar_variaveis.py
@@ -0,0 +1,11 @@
+nome = "Joel"
+idade = 26
+profissao = "desenvolvedor"
+linguagem = "Python"
+
+print("Nome: %s Idade: %d" %(nome, idade))
+
+print("Nome: {} Idade: {}" .format(nome, idade))
+print("Nome: {nome} Idade: {idade}" .format(nome = nome, idade = idade))
+
+print(f"Nome: {nome} Idade: {idade}" .format(nome, idade))
---
+++ aulas/manipulando_strings/manipulando_string.py
@@ -0,0 +1,34 @@
+nome = "PaRauAPebAs"
+
+print(nome.upper())
+print(nome.lower())
+print(nome.title())
+
+
+
+#TIRANDO OS ESPAÇOS
+
+texto = ("            Olá, Mundo!           ")
+
+print(texto + ".")
+print(texto.strip() + ".")
+print(texto.rstrip() + ".")
+print(texto.lstrip() + ".")
+
+
+
+#JUNÇÃO E CENTRALIZAÇÃO
+
+menu = "Python"
+print("####" + menu + "####")
+
+print(menu.center(14))
+print(menu.center(14, "#"))
+print("-".join(menu))
+
+
+
+
+
+
+
