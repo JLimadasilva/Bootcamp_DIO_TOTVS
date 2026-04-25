5167ac378cde191668e5bab312be4873b6faf6ff
Joel Lima da Silva <105734951+JLimadasilva@users.noreply.github.com>
2026-04-24 22:22:38 -0300

    Adicionando estudoes de listas em Python

---
+++ aulas/listas/declarando_tipos_listas.py
@@ -0,0 +1,14 @@
+frutas = ["laranja", "maçã", "uva"]
+print(frutas)
+
+frutas = []
+print(frutas)
+
+letras = list("Python")
+print(letras)
+
+numeros = list(range(10))
+print(numeros)
+
+carro = ["ferrari", "F8", 123000000, 2020, 2900, "Belem", True]
+print(carro)
