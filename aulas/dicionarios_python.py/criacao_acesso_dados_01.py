d48a22aa9d8795ccabfb563480804e047f2eb145
Joel Lima da Silva <105734951+JLimadasilva@users.noreply.github.com>
2026-04-28 22:30:58 -0300

    Adicionando estudo de dicionarios em Python

---
+++ aulas/dicionarios_python.py/criacao_acesso_dados_01.py
@@ -0,0 +1,7 @@
+pessoa = {"nome": "Joel", "idade": 26}
+
+pessoa = dict(nome = "Joel", idade = 26)
+
+pessoa["telefone"] = "91983257611"
+
+print(pessoa["nome"])
---
+++ aulas/dicionarios_python.py/dicionario_aninhado.py
@@ -0,0 +1,9 @@
+contatos = {
+    "joellima@emmail.com": {"nome": "Joel", "telefone": "9198325-7611"},
+    "nicole@emmail.com": {"nome": "nicole", "telefone": "91988765-7611"},
+    "marcia@emmail.com": {"nome": "Narcia", "telefone": "9192355-7611"},
+    "jairo@emmail.com": {"nome": "jairo", "telefone": "9198325-7871"},
+    "nicolas@emmail.com": {"nome": "Nicolas", "telefone": "9176565-7611"}
+}
+
+print(contatos["joellima@emmail.com"]["telefone"])
